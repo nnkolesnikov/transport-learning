b1f4a2d6549f2920798fddd95d72c8fefd3f23a8
nnkolesnikov <kolesnikov.nn@protonmail.com>
2020-03-04 20:03:29 +0300

    ref

--- pkg/service/httpserver/preset.go
+++ pkg/service/httpserver/preset.go
@@ -3,18 +3,13 @@ package httpserver
 const (
 	URIPrefix = "/api/v1"
 
-	URIPathClientGetUser = URIPrefix + "/some/%s"
-	URIPathClientGetOrders = URIPrefix + "/some/%s"
-	URIPathClientGetUserCount = URIPrefix + "/some/%s"
-	URIPathClientGetOrdersWithoutParams = URIPrefix + "/some/%s"
-
-	URIPathGetUser = URIPrefix + "/user"
-	URIPathGetOrders = URIPrefix + "/orders"
-	URIPathGetUserCount = URIPrefix + "/user/:id/count"
+	URIPathGetUser                = URIPrefix + "/user"
+	URIPathGetOrders              = URIPrefix + "/orders"
+	URIPathGetUserCount           = URIPrefix + "/user/:id/count"
 	URIPathGetOrdersWithoutParams = URIPrefix + "/orders"
 
-	HTTPMethodGetUser = "GET"
-	HTTPMethodGetOrders = "POST"
-	HTTPMethodGetUserCount = "GET"
+	HTTPMethodGetUser                = "GET"
+	HTTPMethodGetOrders              = "POST"
+	HTTPMethodGetUserCount           = "GET"
 	HTTPMethodGetOrdersWithoutParams = "GET"
 )
