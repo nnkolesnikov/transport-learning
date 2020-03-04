77b478d98e93d76a89195fdab235eaa63106a094
nnkolesnikov <kolesnikov.nn@protonmail.com>
2020-03-04 20:39:07 +0300

    create preset for prepared client

---
+++ pkg/service/httpclient/preset.go
@@ -0,0 +1,15 @@
+package httpclient
+
+const (
+	URIPrefix = "/api/v1"
+
+	URIPathClientGetUser                = URIPrefix + "/user"
+	URIPathClientGetOrders              = URIPrefix + "/orders"
+	URIPathClientGetUserCount           = URIPrefix + "/user/:id/count"
+	URIPathClientGetOrdersWithoutParams = URIPrefix + "/orders"
+
+	HTTPMethodGetUser                = "GET"
+	HTTPMethodGetOrders              = "POST"
+	HTTPMethodGetUserCount           = "GET"
+	HTTPMethodGetOrdersWithoutParams = "GET"
+)
